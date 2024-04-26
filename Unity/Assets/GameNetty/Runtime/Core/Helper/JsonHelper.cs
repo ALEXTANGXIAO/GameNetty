08de5102b7f0ec184400d62fae4d8dba58d01b32
ALEXTANG <574809918@qq.com>
2024-04-26 17:48:22 +0800

    Add MailBoxHanlder & MessageHandler

---
+++ Unity/Assets/GameNetty/Runtime/Core/Helper/JsonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ET
+{
+    public static class JsonHelper
+    {
+        public static string ToJson(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static T ToObject<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public static object ToObject(Type objectType, string json)
+        {
+            return JsonConvert.DeserializeObject(json);
+        }
+    }
+}
