2fd077eb6d45406c6f65a81800d41bf3e2ba725f
ALEXTANG <574809918@qq.com>
2024-04-26 17:48:47 +0800

    Update Options.cs

--- Unity/Assets/GameNetty/Runtime/Core/World/Module/Option/Options.cs
+++ Unity/Assets/GameNetty/Runtime/Core/World/Module/Option/Options.cs
@@ -1,7 +1,21 @@
 ﻿namespace ET
 {
-    public class Options: Singleton<Options>
+    public class Options
     {
+        private static Options _instance;
+
+        public static Options Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Options();
+                }
+                return _instance;
+            }
+        }
+        
         public int Process { get; set; } = 1;
     }
 }
