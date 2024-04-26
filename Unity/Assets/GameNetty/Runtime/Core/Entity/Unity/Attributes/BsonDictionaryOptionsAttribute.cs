c6b905855f8acf6cc84b470d803af4de3bb1b614
ALEXTANG <574809918@qq.com>
2024-04-26 17:47:46 +0800

    Add BsonAttributes

---
+++ Unity/Assets/GameNetty/Runtime/Core/Entity/Unity/Attributes/BsonDictionaryOptionsAttribute.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+using System;
+using MongoDB.Bson.Serialization.Options;
+
+namespace MongoDB.Bson.Serialization.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class BsonDictionaryOptionsAttribute : Attribute
+    {
+        private DictionaryRepresentation _representation = DictionaryRepresentation.Document;
+
+        public BsonDictionaryOptionsAttribute()
+        {
+        }
+
+        public BsonDictionaryOptionsAttribute(DictionaryRepresentation representation)
+        {
+            this._representation = representation;
+        }
+    }
+}
---
+++ Unity/Assets/GameNetty/Runtime/Core/Entity/Unity/Attributes/DictionaryRepresentation.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+namespace MongoDB.Bson.Serialization.Options
+{
+    public enum DictionaryRepresentation
+    {
+        Document,
+        ArrayOfArrays,
+        ArrayOfDocuments,
+    }
+}
