bf93aac5c52e2ea59be1d408ecfc3aa17ccea16c
ALEXTANG <574809918@qq.com>
2024-04-30 11:39:15 +0800

    打协议工具

---
+++ Unity/Assets/GameNetty/Editor/ToolsEditor/ToolsEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+
+namespace ET
+{
+    public static class ToolsEditor
+    {
+        public static void ExcelExporter()
+        {
+#if UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
+            const string tools = "./Tool";
+#else
+            const string tools = ".\\Tool.exe";
+#endif
+            ShellHelper.Run($"{tools} --AppType=ExcelExporter --Console=1", "../Bin/");
+        }
+
+        private const string clientMessagePath = "../Unity/Assets/GameScripts/GameProto/Generate/Message/";
+        
+        private const string serverMessagePath = "../Server/Model/Generate/Message/";
+
+        private const string protoDir = "../Config/Proto";
+        
+        [MenuItem("ET/Build Proto2CS")]
+        public static void Proto2CS()
+        {
+#if UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
+            const string tools = "./Tool";
+#else
+            const string tools = ".\\Tool.exe";
+#endif
+            // ShellHelper.Run($"{tools} --AppType=Proto2CS --Console=1 {protoDir} {clientMessagePath} {serverMessagePath}", "../Bin/");
+            ShellHelper.Run($"{tools} --AppType=Proto2CS --Console=1", "../Bin/");
+        }
+    }
+}
